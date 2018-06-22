177d6d2304303534f9254e119ae2969044c156d4
MartiGaldi <martina.galdi46@gmail.com>
2018-06-23 01:03:34 +0200

    added new folder Controller with c_cerca

---
+++ Controller/c_cerca.php
@@ -0,0 +1,108 @@
+<?php
+
+//La classe c_cerca implementa la funzionalità di Ricerca all'interno del catalogo
+
+class c_cerca
+
+{
+    //default
+    const KEY_DEFAULT = 'Titolo';
+
+    //avanzata
+    const KEY_ADVANCED = 'Autore';
+    const KEY_ADVANCED = 'Genere';
+    const KEY_ADVANCED = 'Isbn';
+    
+    
+    
+   //Metodo implementa la ricerca di base che può essere effettuata da qualsiasi tipo di utente
+    
+    static function ricecaBase()
+    
+    {
+        
+        $v_ricerca = new v_ricerca();
+        
+        $utente = c_session::getUtenteDaSessione();
+        
+        
+        $string = $v_cerca->getValoreRicerca();
+        
+        
+        
+        if($string)
+        
+        {   
+            $oggetto = f_persistance::getInstance()->cerca(c_cerca::KEY_DEFAULT, $string);
+            
+            $v_cerca->mostraRisultato($utente, $oggetto, c_cerca::KEY_DEFAULT, $string);
+            
+        }
+        
+        else
+            $v_cerca->Errore($user, 'La ricerca non ha prodotto risultati');
+    }
+    
+    
+    
+ 
+    
+    //Questo metodo implementa la ricerca avanzata che può essere effettuata solo dal cliente e dal bibliotecario
+    
+    static function avanzata()
+    
+    {
+        
+        $v_cerca = new v_cerca();
+        
+        $utente = c_sessione::getUserDaSessione();
+        
+        
+        
+        if (get_classe($utente) != e_visitatore::class)
+        
+        {   
+            //stringa utente
+            $string = $v_cerca->getValoreRicerca();
+            
+            
+            
+            if ($string)
+            
+            { // si ricavano chiave e valore di ricerca scelti dall'utente
+                
+                list($key)=$v_cerca->getKey();
+                
+                if(($key == c_cerca::KEY_DEFAULT || $key == c_cerca::KEY_ADVANCED)
+                
+                {   
+                    $oggetti = f_peristance::getInstance()->cerca($key, $string);
+                    
+                    $v_cerca->mostraRisultato ($user, $oggetti, $key, $string);
+                    
+                }
+                
+                else
+                    $v_cerca-> Errore($user, 'I valori inseriti non sono corretti');
+                    
+            }
+            
+            else
+            
+            {
+                //se la stringa non e' stata inserita, l'utente si reindirizza alla pagina della ricerca avanzata
+                $v_cerca->mostraRicercaAvanzata($utente);
+                
+            }
+            
+        }
+        
+        else
+        // se l'utente e' un visitatore, si reindirizza ad una pagina di errore
+            
+            $v_cerca->Errore($utente, 'Devi essere registrato per accedere a questa funzionalità');
+            
+    }
+    
+}
+
--- inc.php
+++ inc.php
@@ -3,24 +3,28 @@
 require_once 'Entity/e_cliente.php';
 require_once 'Entity/e_libro.php';
 require_once 'Entity/e_prenota.php';
-require_once 'Entity/e_id.php';
 require_once 'Entity/e_commento.php';
-require_once 'Entity/e_infoCliente.php';
+require_once 'Entity/e_infoUtente.php';
 require_once 'Entity/e_infoLibro.php';
 require_once 'Entity/e_copertina.php';
 require_once 'Entity/e_prestito.php';
+require_once 'Entity/e_oggetto.php';
+require_once 'Entity/e_utente.php';
 require_once 'Entity/e_visitatore.php';
 
 
-
-require_once 'Foundation/f_clientphp';
+require_once 'Foundation/f_cliente.php';
 require_once 'Foundation/f_persistance.php';
 require_once 'Foundation/f_prenota.php';
 require_once 'Foundation/f_libro.php';
 require_once 'Foundation/f_prestito.php';
 require_once 'Foundation/f_commento.php';
-require_once 'Foundation/f_infoCliente.php';
+require_once 'Foundation/f_infoUtente.php';
 require_once 'Foundation/f_infoLibro.php';
+require_once 'Foundation/f_bibliotecario.php';
 require_once 'Foundation/f_copertina.php';
+require_once 'Foundation/f_oggetto.php';
+require_once 'Foundation/f_utente.php';
+require_once 'Foundation/f_visitatore.php';
 
 ?>
