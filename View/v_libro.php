5f8c461adce7e28c70bb86e9451ad26349a86e6e
MartiGaldi <martina.galdi46@gmail.com>
2018-07-04 17:14:57 +0200

    added v_libro and v_utente in View

---
+++ View/v_libro.php
@@ -0,0 +1,191 @@
+<?php
+require_once 'inc.php';
+include_once 'View/v_oggetto.php';
+
+
+/**
+* La classe v_libro si occupa dell'input-output per quanto riguarda i dati riguardanti i libri.
+*  Ovvero:
+* - dai dati inseriti dal bubliotecario costruiscono un oggetto e_libro
+* - predispone e visualizza le varie pagine HTML per la creazione/modifica/rimozione/visualizzazione di un testo.
+* @package View
+*/
+class v_libro extends v_oggetto
+{
+    
+    /**
+    * Costruttore che inizializza il componente view e definisce l'array contenente gli errori
+    * che possono essere commessi nella form di caricamento/modifica testi
+    */
+    
+    function __construct()
+    {
+        parent::__construct();
+        
+        // l'array è istanziato con indici i campi delle varie form, i cui valori sono di default a false (no errori)
+        $this->check = array(
+            'titolo' => true,
+            'n_copie' => true,
+            'autore' => true,
+            'isbn' => true,
+        ); 
+    }
+    
+    
+
+    /**
+    * Funzione che crea un testo a partire dagli input della form.
+    * @return e_libro
+    */
+    
+    function creaLibro() : e_libro
+    {
+        $libro = new e_libro();
+        if (isset($_POST['titolo']) && isset($_POST['n_copie']) && isset($_POST['autore']) && isset($_POST['isbn']))
+        {
+            // impostazione di titolo,autore,isbn e numero copie
+            $libro->setTitolo(ucfirst($_POST['titolo']));
+            $libro->setNumCopie(ucfirst($_POST['n_copie']));
+            $libro->setAutore(ucfirst($_POST['autore']));
+            $libro->setIsbn(ucfirst($_POST['isbn']));                
+        }        
+        return $libro;
+        
+    }
+    
+    
+    
+    /**
+    * Mostra la pagina che consente l'inserimento di un brano da parte di un bibliootecario
+    * @param e_utente $utente l'utente della sessione
+    * @param bool $errore facoltativo, da impostare a true se il biblitecario ha inserito
+    * un titolo di un testo gia' inserita da lui
+    */
+    
+    function mostraFormCarica (e_utente &$utente, bool $errore = NULL)
+    {
+        if (! $error) 
+           $error = false;
+            
+            $this->smarty->registerObject('utente', $utente);
+            $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+            
+            $this->smarty->assign('errore', $errore);
+            $this->smarty->assign('check', $this->check);
+            
+            $this->smarty->display('libro/caricaLibro.tpl');   
+    }
+    
+    
+    
+    /**
+    
+    * Mostra la pagina che consente la modifica di un testo da parte di un bibliotecario
+    * @param e_utente $utente l'utente della sessione
+    * @param e_libro $lirbo il testo da modificare
+    * @param bool $error facoltativo, da impostare a true se l'utente ha inserito un titolo di una libro
+    * gia' inserito
+    */
+    
+    function mostraFormModifica (e_utente &$utente, e_libro &$libro, bool $errore= NULL)
+    {
+        if (! $error)
+        { $error = false;}
+            
+            $this->smarty->registerObject('utente', $utente);
+            $this->smarty->assign('libro', $libro);
+                    
+            $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+            $this->smarty->assign('errore', $errore);  
+                     
+            $this->smarty->display('libro/modificaLibro.tpl');      
+    }
+    
+    
+    
+    /**
+    * Mostra la pagina che consente la rimozione di un testo da parte di un bibliotecario
+    * @param e_utente $utente: l'utente della sessione
+    * @param e_libro $libro: il testo da rimuovere
+    */
+    
+    function mostraFormRimuovi(e_utente &$utente, e_libro &$libro)
+    {
+        $this->smarty->registerObject('utente', $utente);
+        $this->smarty->assign('libro', $libro);
+        
+        $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+        $this->smarty->display('libro/rimuoviLibro.tpl');
+        
+    }
+    
+    
+    
+    
+    /**
+    * Mostra il contenuto di un libro.
+    * @param e_utente $utente l'utente che sta visualizzando la pagina
+    * @param e_libro $libro il libro da visualizzare
+    */
+    
+    function mostraLibro(e_utente &$utente, e_libro &$libro)
+    {
+        $this->smarty->registerObject('utente', $utente);
+        $this->smarty->assign('libro', $libro);
+
+        $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+        
+        $this->smarty->display('libro/libro.tpl');  
+    }
+    
+    
+    
+    /**
+    * Funzione che controlla i campi della form per l'inserimento di un libro
+    * @return bool true se gli input sono corretti, false altrimenti
+    */
+    
+    function validazioneCarica(e_libro &$libro) : bool
+    {
+        if($this->check['titolo']=$libro->validazioneTitolo() && $this->check['n_copie']=$libro->validazioneNumCopie() && $this->check['autore']=$libro->validazioneAutore() && this->check['isbn']=$libro->validazioneIsbn())
+            return true;
+        
+            else
+                return false;  
+    }
+    
+    
+    
+    /**
+    * Funzione che controlla i campi della form per la modifica di un testo
+    * @return bool true se gli input sono corretti, false altrimenti
+    */
+    function validazioneModifica(e_libro &$libro) : bool
+    {
+        if($this->check['titolo']=$libro->validazioneTitolo() && $this->check['n_copie']=$libro->validazioneNumCopie() && $this->check['autore']=$libro->validazioneAutore() && this->check['isbn']=$libro->validazioneIsbn())
+            return true;
+        
+        else 
+            return false;      
+    }
+    
+    
+    
+    /**
+    * Funzione che verifica se l'utente ha effettivamente richiesto la rimozione di una canzone
+    * @return bool true se l'utente vuole rimuovere il brano, false altrimenti
+    */
+    function validazioneRimuovi() : bool
+    {
+        if(isset($_POST['action']))
+        {
+            if($_POST['action']=='yes')
+                return true;
+                
+                else
+                    return false;       
+        }
+        else
+            return false;   
+    }
+}
---
+++ View/v_utente.php
@@ -0,0 +1,169 @@
+<?php
+require_once 'inc.php';
+include_once 'View/v_oggetto.php';
+
+/**
+* La classe v_utente si occupa dell'input-output per quanto riguarda la gestione di un utente.
+* In particolare:
+* - Costruisce da una form un oggetto e_utente e ne verifica la validità
+* - Permette al client di visualizzare pagine relative all'utente (accedi-registrati-profilo)
+* @package View
+*/
+
+class v_utente extends v_oggetto
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+
+        
+        $this->check = array(
+            'mail' => true,
+            'password' => true,
+            'tipo' => true 
+        ); 
+    }
+    
+    
+    
+    /**
+    * Funzione che permette la creazione di utente con i valori prelevati da una form
+    * @return e_utente l'utente ottenuto dai campi della form
+    */
+    
+    function creaUtente() : e_utente
+    { 
+        $utente;
+        
+        if(isset($_POST['tipo']))
+        { 
+            $tipo = 'e_'.lcfirst($_POST['tipo']);
+            $utente = new $tipo(); 
+        }
+        
+        else
+            $utente = new e_utente();
+                
+                if(isset($_POST['mail']))
+                    $utente->setMail($_POST['mail']);
+                    
+                    if(isset($_POST['password']))
+                        $utente->setPassword($_POST['pwd']);
+                        
+                        return $utente;                
+    }
+   
+    
+    
+    /**
+    * Verifica che un utente abbia rispettato i vincoli per l'inserimento dei parametri di login
+    * @return true se non si sono commessi errori, false altrimenti
+    */
+    function validazioneLogin(e_utente $utente): bool
+    {
+        if($this->check['mail']=$utente->validazioneMail() && $this->check['password']=$utente->validazionePassword())
+            return true;
+        
+        else
+            return false;    
+    }
+    
+    
+   
+    
+    /**
+    * Mostra il profilo di un utente.
+    * @param e_utente $profiloUtente: l'utente di cui visualizzare il profilo
+    * @param e_utente $Utente: l'utente che ha effettuato l'accesso alla sessione
+    * @param string $contenuto: il contenuto da visualizzare nel profilo (Libri)
+    * @param array $array: l'array del contenuto da visualizzare
+    */
+    
+    function mostraProfilo (e_utente &$profiloUtente, e_utente &$Utente, string $contenuto, array $array = NULL)
+    {
+        $this->smarty->assign('contenuto', $contenuto);
+        $this->smarty->registerObject('utente', $Utente);
+        $this->smarty->assign('uTipo', lcfirst(substr(get_class($User), 2)));
+        
+        $this->smarty->registerObject('profilo', $profiloUtente);
+        $this->smarty->assign('pTipo', lcfirst(substr(get_class($profiloUtente), 2)));
+      
+        $this->smarty->assign('array', $array);
+        
+        $this->smarty->display('utente/profilo.tpl'); 
+    }
+    
+    
+    
+    /**
+    * Mostra la pagina di login
+    * @param bool $errore: facoltativo se è stato rilevato un errore
+    */
+    
+    function mostraLogin(bool $errore = NULL)
+    {
+        if(!$errore)
+        {$errore = false;}
+            
+            $utente = new e_visitatore();
+            
+            $this->smarty->registerObject('utente', $utente);
+            $this->smarty->assign('uTipo', lcfirst(substr(get_class($uteente), 2)));
+            
+            $this->smarty->assign('errore', $errore);
+            $this->smarty->assign('check', $this->check);
+            
+            $this->smarty->display('utente/login.tpl');     
+    }
+    
+    
+    
+    /**
+    * Mostra la pagina di iscrizione
+    * @param bool $error: facoltativo se e' stato rilevato un errore
+    */
+    
+    function mostraIscrizione(bool $errore = NULL)
+    {
+        if (! $errore) 
+        {$errore = false;}
+            
+            $utente = new e_visitatore(); 
+            $this->smarty->registerObject('utente', $utente);
+            $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+            
+            $this->smarty->assign('errore', $errore);
+            $this->smarty->assign('check', $this->check);
+
+            $this->smarty->display('utente/registrati.tpl'); 
+    }
+    
+    
+    
+    /**
+    * Mostra la pagina che consente la rimozione di un utente
+    * @param e_utente $utente: l'utente della sessione
+    * @param e_utente $rimuovi: se l'utente che ha richiesto la rimozione e' un bibliotecario
+    */
+    
+    function mostraFormRimuovi (e_utente &$utente, e_utente &$rimuovi = null)
+    {
+        $this->smarty->registerObject('utente', $utente);
+        $this->smarty->assign('uTipo', lcfirst(substr(get_class($utente), 2)));
+        
+        if($rimuovi)
+        {
+            $setRimuoviUtente = true;
+            $this->smarty->assign('rName', $removed->getNickName());
+            $this->smarty->assign('rId', $removed->getId());
+        }
+        else
+        {
+            $this->smarty->assign('rNome', NULL);
+            $this->smarty->assign('rId', NULL);
+        }
+        
+        $this->smarty->display('utente/remuoviUtente.tpl');
+    }  
+}
