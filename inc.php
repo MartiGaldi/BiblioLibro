64243df4a4c80422bd17f77ae925575391f8c478
MartiGaldi <martina.galdi46@gmail.com>
2018-11-22 12:22:08 +0100

    update

--- inc.php
+++ inc.php
@@ -6,7 +6,6 @@ require_once 'SampleUsers.php';
 require_once 'Entity/e_cliente.php';
 require_once 'Entity/e_libro.php';
 require_once 'Entity/e_prenota.php';
-require_once 'Entity/e_infoLibro.php';
 require_once 'Entity/e_copertina.php';
 require_once 'Entity/e_prestito.php';
 require_once 'Entity/e_utente.php';
@@ -19,8 +18,6 @@ require_once 'Foundation/f_persistance.php';
 require_once 'Foundation/f_prenota.php';
 require_once 'Foundation/f_libro.php';
 require_once 'Foundation/f_prestito.php';
-
-require_once 'Foundation/f_infoLibro.php';
 require_once 'Foundation/f_copertina.php';
 require_once 'Foundation/f_utente.php';
 require_once 'Foundation/f_target.php';
@@ -29,7 +26,6 @@ require_once 'Foundation/f_storicoPrestito.php';
 
 require_once 'Controller/c_amministratore.php';
 require_once 'Controller/c_ricerca.php';
-require_once 'Controller/c_infoLibro.php';
 require_once 'Controller/c_libro.php';
 require_once 'Controller/c_sessione.php';
 require_once 'Controller/c_utente.php';
@@ -38,11 +34,9 @@ require_once 'Controller/FrontController.php';
 
 require_once 'View/v_amministratore.php';
 require_once 'View/v_ricerca.php';
-
 require_once 'View/v_oggetto.php';
 require_once 'View/v_libro.php';
 require_once 'View/v_utente.php';
-require_once 'View/v_infoLibro.php';
 
 require_once 'SmartyConfig.php';
 
